b02be30b6b76b9a022d78550f58d4242c5ab13b1
Anthony Zheng <anthony.zheng@epita.fr>
2024-01-13 15:31:02 +0100

    feat: .h

---
+++ src/builtin/builtin.h
@@ -0,0 +1,11 @@
+#ifndef BUILTIN_H
+#define BUILTIN_H
+
+int truef(void);
+int falsef(void);
+int space(char *ec, int pos, int len);
+int len(char **ec);
+void prt(char **ec, int pl, int eE);
+void echo(char **ec);
+
+#endif
