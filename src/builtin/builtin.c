fcd8fa7870d97605de4e1828e2228c061d99a7b4
Anthony Zheng <anthony.zheng@epita.fr>
2024-01-30 20:37:58 +0100

    fix(echo): dont skip

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -111,8 +111,6 @@ static void prt(char **argv, int pl, int eE)
     pl++;
     while (pl < len(argv))
     {
-        if (argv[pl][0] == '\0')
-            continue;
         printf(" ");
         if (eE)
             printf("%s", argv[pl]);
