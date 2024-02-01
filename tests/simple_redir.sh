9e798734e34609f3f173cac39577ddb7c242d82f
David Xu <david.xu@epita.fr>
2024-02-01 20:35:34 +0100

    more test

---
+++ tests/simple_redir.sh
@@ -0,0 +1,20 @@
+echo tot > tot.txt
+echo tot >> tot.txt
+cat < tot.txt
+cat 0< tot.txt
+rm tot.txt
+echo tot >> tot.txt
+echo tot 2>> tot.txt
+echo tot 1> tot.txt
+
+rm tot.txt
+
+echo tot >&1 tot.txt
+echo tot >&2 tot.txt
+echo tot 1>&2 tot.txt
+echo tot 1>| tot.txt
+echo tot >| tot.txt
+
+cat <&1 tot.txt
+cat 0<&1 tot.txt
+
